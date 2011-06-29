b79be3df660cfb99d2b885fb77327597d051cf9c
Gergely Imreh <imrehg@gmail.com>
2011-06-29 12:38:24 +0800

    Actual working driver with example code

--- tekdriver.py
+++ tekdriver.py
@@ -1,153 +1,103 @@
 #!/usr/bin/env python
+"""
+Monitoring and controlling Tektronix TDS1012B over USBTMC
+Basic driver and example code
 
-# Monitoring and controlling Tektronix TDS1012B over USBTMC
+Needs Python 2.x at the moment
+"""
 
 from __future__ import division
-import os
-from time import time, sleep
-import string
-import re
 import pylab
-from scipy import *
+
+DEBUG = True
 
 class Scope:
+    """
+    Tektronix oscilloscope driver
+    Tested with TDS 1012B
+    """
 
     def __init__(self, location=None):
+        """ Initialize driver, default location is first usbtmc device """
         if not location:
             location = "/dev/usbtmc0"
         self.handle = open(location, "r+")
 
+    def send(self, cmd):
+        """ Send a command to the device """
+        if DEBUG:
+            print("Send: %s" %(cmd))
+        # Terminate with line-feed
+        self.handle.write("%s\n" %(cmd))
 
-    def sendCmd(self, cmd):
-        print "Query: %s" %(cmd)
-        try:
-            handle.write("%s\n" %(cmd))
-        except:
-            error = True
+    def recv(self, timeout=5):
+        """ Receive a text response from device """
+        self.handle.seek(0, 0)
+        return self.handle.readline().strip()
 
-    def recvCmd(self, timeout=5):
-       starttime = time()
-       while (time() - starttime) < timeout:
-          try:
-             self.handle.seek(0, 0)
-             data = self.handle.read(2048)
-             print data
-             return data
-             break
-          except:
-             continue
-       return ""
+    def query(self, cmd):
+        """ Ask and listen for response in one go, for a query command """
+        self.send(cmd)
+        return self.recv()
 
-    def ask(self, cmd):
-        self.sendCmd(cmd)
-        return self.recvCmd()
+    def getdata(self, channel=1):
+        """
+        Data acquisition from scope
 
+        Input:
+        channel: [1/2] which channel to query
 
+        """
+        size = 2500
+        width = 1 # Scope internal data structure uses 8-bit data, here no gain to use width=2
+        self.send("DATA:SOURCE CH%d;WIDTH %d;ENCDG RPB;STOP %d" % (channel,width,size))
+        self.send("CURVE?")
+        tmp = self.handle.read(2)
+        digits = int(tmp[1])
+        tmp = self.handle.read(digits)
+        bytes = int(tmp)
+        data = self.handle.read(bytes)
 
-
-new = Scope()
-new.sendCmd("CH1:scale?")
-print new.recvCmd()
-
-# #### Setup
-# scopedev = "/dev/usbtmc0"
-# handle = open(scopedev, "r+")
+        # On-screen scale -5..+5 division: 1..255
+        tonumber = lambda x: ord(x) - 128
+        datanum = [tonumber(x) for x in data]
+        return datanum
 
 
-# def loopplot(handle):
-#     if (not error):
-#         try:
-#             # Get voltage scale
-#             sendCmd(handle,"CH1:scale?")
-#             data = recvCmd(handle,5).strip()
-#             print "Reply: %s" %(data)
-#             vscale = float(data)
 
-#             # Get time scale
-#             sendCmd(handle,"HOR:scale?")
-#             data = recvCmd(handle,5).strip()
-#             print "Reply: %s" %(data)
-#             hscale = float(data)
+if __name__ == "__main__":
+    # Example: data acquisition
+    # Some of this might be moved into the Scope class later for convenience
 
-#             sendCmd(handle,"acquire:stopafter runstop")
-#             sendCmd(handle,"acquite:state run")
+    ####### Settings:
+    ####Good values: 1, 2
+    channel = 1
+    ####
 
-#             sendCmd(handle,"MEASUrement:IMMed:TYPe Frequency")
-#             sendCmd(handle,"MEASUrement:IMMed:Value?")
-#             data = recvCmd(handle,5).strip()
-#             print "Reply: %s" %(data)
-#             freq = float(data)
+    scope = Scope()
+    id = scope.query("*IDN?")
+    print("Scope ID string: %s" %(id))
+    
+    print("Using channel %d" %(channel))
 
-#             size=2500
-#             width=1
-#             sendCmd(handle,"data:source ch1;width %d;ENCdg RPB;stop %d" % (width,size))
+    hscale = float(scope.query("HOR:scale?"))
+    vscale = float(scope.query("CH%d:scale?" %(channel)))
+    print("X-scale: %g s, Y-scale: %g V" %(hscale, vscale))
 
-#             sendCmd(handle,"curve?")
-#             data = recvCmd(handle,5)
-#             dd = int(data[1])
-#             num = int(data[2:2+dd])
-#             totalnum = 2 + dd + num
-#             print "Number of points: %d" %(num)
-#             while (len(data) < totalnum):
-#                 moredata = recvCmd(handle,5)
-#                 if (len(moredata) > 0):
-#                     data += moredata
-#                 else:
-#                     break
-#             data = data.strip()
-#             data = data[(totalnum-num):]
-#             print "Data-length: %d " % (len(data))
-#             return
+    zeropoint = float(scope.query("CH%d:POS?" %(channel)))
+    print("Vertical center: %g divisions" %(zeropoint))
+    
+    # This scaling will be okay for continuous scan and stopped sequence,
+    # but wrong if a stopped sequence is vertically shifted by device front knob
+    vscaling = lambda x: ((x / 256 * 10) - zeropoint) * vscale
+    data = scope.getdata(channel=channel)
+    scaleddata = [vscaling(x) for x in data]
 
-#             out = zeros(num/width)
-#             if (width == 1):
-#                 for i in range(0, len(data)):
-#                     out[i] = ord(data[i]) - 128
-#             else:
-#                 for i in range(0, len(data), 2):
-#                     out[int(i/2)] = (ord(data[i])*256 + ord(data[i])) - 32768
+    timing = lambda t: t / 2500 * 10 * hscale
+    datatime = [timing(t) for t in xrange(2500)]
 
-#             xlim = hscale*10;
-#             x = linspace(0,xlim,size) * 1000
-
-#             y = out / 128 * 5 * vscale * 1000;
-
-#             fig = pylab.figure()
-#             pylab.plot(x,y,'-')
-
-#             pylab.xlabel("Time (ms)")
-#             pylab.ylabel("Voltage (mV)")
-
-#             if (freq/1e6 > 1):
-#                 fvalue = freq / 1e6
-#                 funit = "MHz"
-#             elif (freq/1e3 > 1):
-#                 fvalue = freq / 1e3
-#                 funit = "kHz"
-#             else:
-#                 fvalue = freq
-#                 funit = "Hz"
-
-#             pylab.title('Measured frequency: %f %s' % (fvalue, funit))
-#             pylab.grid(True)
-
-#             pylab.savefig("/tmp/scope.png", format="png")
-
-#         except e:
-#             raise e
-#         except (KeyboardInterrupt):
-#             return 101
-#         except:
-#             return 1
-
-#         return 0
-
-# while True:
-#     ret = loopplot(handle)
-#     if ret == 101:
-#         break
-#     sleep(2)
-
-# ### Cleaning up
-# handle.close()
-# handle = -1
+    pylab.plot(datatime, scaleddata)
+    pylab.title("Channel %d" %(channel))
+    pylab.xlabel("Time (s)")
+    pylab.ylabel("Voltage (V)")
+    pylab.show()
